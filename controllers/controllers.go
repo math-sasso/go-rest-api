aff044f4bacde7356fe8012f319d7bae0e91c2af
math-sasso <matheus.sasso17@gmail.com>
2023-02-09 11:19:10 -0300

    feat: lecture 2 finished

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/math-sasso/go-rest-api/models"
 )
 
@@ -14,4 +16,15 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(models.Personalidades)
+}
+
+func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	for _, personlidade := range models.Personalidades {
+		if strconv.Itoa(personlidade.Id) == id {
+			json.NewEncoder(w).Encode(personlidade)
+		}
+
+	}
 }
